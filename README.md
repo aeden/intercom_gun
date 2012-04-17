a7f3ec96d6eb2a17f1bac6e03d770cba64fa612b
Anthony Eden <anthonyeden@gmail.com>
2012-04-17 22:25:48 +0200

    Hook up to intercom API.

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ Get the app and do this:
 
     gem install bundler
     bundle install
+
+## Deployment on Heroku
+
+    heroku create appname
+    heroku config:add INTERCOM_APP_ID=app-id
+    heroku config:add INTERCOM_APP_KEY=app-key
+    git push heroku master
--- app.rb
+++ app.rb
@@ -1,5 +1,20 @@
 require 'sinatra'
+require './intercom'
 
 get '/' do
   "Usage instructions will appear here"
+end
+
+post '/:appid/messages' do
+  if params[:appid] == 'dnsimple'
+    message = Hashie::Mash.new(
+      :from => params[:"from"],
+      :subject => params[:"subject"],
+      :body => params[:"stripped-text"]
+    )
+    Intercom.post("/v1/users/message_threads", :body => {
+      :email => message.from,
+      :body => message.body
+    })
+  end
 end
